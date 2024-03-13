471a7c0b63d89eb7077ad68cc0a0996c7f0ba67b
Floris de Boer <florisblog.com@gmail.com>
2024-03-13 11:16:24 +0100

    Tryout

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,7 @@ app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
+app.config['WTF_CSRF_EXPIRATION'] = None
 mail = Mail(app)
 # Initialize Flask extensions here
 
